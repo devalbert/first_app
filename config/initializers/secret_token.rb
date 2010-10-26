877522b51797ba9f13230c724d197ff99ed45654
devalbert <iphone.dev.albert@gmail.com>
2010-10-26 14:03:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0ec56d6313f3a8ef9bcd1f46688ae8da5e615a0d54a3b87ffe3bd51f60c7e6b81761679dc6aed563e3389ab6b8e9e94bba7376377da6ccbf18af055ba4e091fc'
